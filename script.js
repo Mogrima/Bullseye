97cce7c8168eff1769763ea2ffc343527c7fb3e1
Mogrima <rebel.rs@yandex.ru>
2024-01-02 07:58:35 +0500

    game class is included in the script file

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+"use strict";
+import { Game } from './src/Game.js';
+
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = '1280';
     canvas.height = '720';
+    ctx.fillStyle = 'pink';
+    const game = new Game(canvas);
+    game.render(ctx);
+
 });
