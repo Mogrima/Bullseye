689623daa96c455c645c6c6482b4b10a19edee4d
Mogrima <rebel.rs@yandex.ru>
2024-01-13 21:47:08 +0500

    added left-right swinging using sine and cosine

--- src/Farefly.js
+++ src/Farefly.js
@@ -3,7 +3,9 @@ import { Particle } from "./Particle.js";
 export class Farefly extends Particle {
     update() {
         this.angle += this.va;
-        this.collisionX += this.speedX;
+        // чтобы частица раскачивалась вправо-влево нужно умножить постоянно
+        // увеличивающийся синус угла на массу 
+        this.collisionX += Math.cos(this.angle) * this.speedX;
         this.collisionY -= this.speedY;
         if (this.collisionY < 0 - this.radius) {
             this.markedForDeletion = true;
